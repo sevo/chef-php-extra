da232d09ccfd4062b36229be68adc11a7edcb033
Thomas Herr <thom1810@users.noreply.github.com>
2014-02-03 19:36:47 -0500

    Installing ppa php 5.x.x

--- recipes/package.rb
+++ recipes/package.rb
@@ -10,21 +10,21 @@ else
   centos_packages = %w{ php php-devel php-cli php-pear }
 end
 
-pkgs = value_for_platform(
-  [ "centos", "redhat", "fedora" ] => {
-    "default" => centos_packages
-  },
-  [ "debian", "ubuntu" ] => {
-    "default" => %w{ php54 php54-devel php54-cli php54-pear }
-  },
-  "default" => %w{ php5-cgi php5 php5-dev php5-cli php-pear }
-)
-
-pkgs.each do |pkg|
-  package pkg do
-    action :install
-  end
+bash "update_php_sources" do
+   cwd "/"
+   code <<-EOH
+        sudo apt-get -y install -y python-software-properties;
+        sudo add-apt-repository ppa:ondrej/php5;
+        sudo apt-get update -y;
+        sudo apt-get install php5 php5-cli php5-dev php5-common php-pear -y;
+     EOH
 end
+
+#pkgs.each do |pkg|
+#  package pkg do
+#    action :install
+#  end
+#end
 
 template "#{node['php']['conf_dir']}/php.ini" do
   source "php.ini.erb"
